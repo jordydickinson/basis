2d0f73cde1c1d758fec02e9c2634df43e5ee455b
Jordy Dickinson <jordy.dickinson@icloud.com>
2021-10-25 14:37:17 -0400

    Add Equatable.Basic

--- basis/interfaces/equatable.ml
+++ basis/interfaces/equatable.ml
@@ -1,5 +1,9 @@
 include Equatable_intf
 
+module Basic (T: Comparable.Basic) = struct
+  let equal a b = Int.equal (T.compare a b) 0
+end
+
 module Infix (T: Basic) = struct
   let ( = ) = T.equal
   let ( <> ) a b = not (a = b)
--- basis/interfaces/equatable.mli
+++ basis/interfaces/equatable.mli
@@ -1,4 +1,7 @@
 include module type of struct include Equatable_intf end
 
+(** Implement the basic interface using a comparable interface. *)
+module Basic (T: Comparable.Basic) : Basic with type t := T.t
+
 (** Create infix operators for equality. *)
 module Infix (T: Basic) : Infix with type t := T.t
