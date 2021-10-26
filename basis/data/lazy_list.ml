3c5e56c26e7e94ab61d9770ad73144dd9c1a91aa
Jordy Dickinson <jordy.dickinson@icloud.com>
2021-10-26 16:25:24 -0400

    Add Lazy_list

---
+++ basis/data/lazy_list.ml
@@ -0,0 +1,120 @@
+type +'a node =
+| Nil
+| Cons of 'a * 'a t
+
+and +'a t = 'a node Lazy.t
+
+let nil = Lazy.from_val Nil
+
+let is_nil xs = match xs with
+| lazy Nil -> true
+| _ -> false
+
+let cons x xs = Lazy.from_val (Cons (x, xs))
+
+let lcons x xs = Lazy.map_val (fun x -> Cons (x, xs)) x
+
+let singleton x = cons x nil
+
+let lsingleton x = Lazy.map_val (fun x -> Cons (x, nil)) x
+
+let hd_opt = function
+| lazy Nil -> None
+| lazy (Cons (x, _)) -> Some x
+
+let hd = function
+| lazy Nil -> failwith "hd nil"
+| lazy (Cons (x, _)) -> x
+
+let tl_opt = function
+| lazy Nil -> None
+| lazy (Cons (_, xs)) -> Some xs
+
+let tl = function
+| lazy Nil -> failwith "tl nil"
+| lazy (Cons (_, xs)) -> xs
+
+let at_opt i =
+  let rec at_opt' i = function
+  | lazy Nil -> None
+  | lazy (Cons (x, xs)) ->
+    if i = 0 then Some x else
+    at_opt' (i - 1) xs
+  in
+  if i < 0 then invalid_arg "negative index";
+  at_opt' i
+
+let at i xs = match at_opt i xs with
+| None -> raise Not_found
+| Some x -> x
+
+let nth_opt xs i = at_opt i xs
+
+let nth xs i = match nth_opt xs i with
+| None -> raise Not_found
+| Some x -> x
+
+let rec fold_left f acc = function
+| lazy Nil -> acc
+| lazy (Cons (x, xs)) -> fold_left f (f acc x) xs
+
+let rec fold_right f xs acc = match xs with
+| lazy Nil -> acc
+| lazy (Cons (x, xs)) -> f x (fold_right f xs acc)
+
+let length xs = fold_left (fun i _ -> i + 1) 0 xs
+
+let rec iter f xs = match xs with
+| lazy Nil -> ()
+| lazy (Cons (x, xs)) -> f x; iter f xs
+
+let iteri f xs =
+  let i = ref 0 in
+  iter (fun x -> f !i x; incr i) xs
+
+let rec map f xs = lazy begin match xs with
+| lazy Nil -> Nil
+| lazy (Cons (x, xs)) -> Cons (f x, map f xs)
+end
+
+let mapi f =
+  let rec mapi' i xs = lazy begin match xs with
+  | lazy Nil -> Nil
+  | lazy (Cons (x, xs)) -> Cons (f i x, mapi' (i + 1) xs)
+  end in
+  mapi' 0
+
+let rec map_val f xs = xs |> Lazy.map_val begin function
+| Nil -> Nil
+| Cons (x, xs) -> Cons (f x, map_val f xs)
+end
+
+let mapi_val f =
+  let rec mapi_val' i xs = xs |> Lazy.map_val begin function
+  | Nil -> Nil
+  | Cons (x, xs) -> Cons (f i x, mapi_val' (i + 1) xs)
+  end in
+  mapi_val' 0
+
+let rec append xs ys = lazy begin match xs with
+| lazy Nil -> Lazy.force ys
+| lazy (Cons (x, xs)) -> Cons (x, append xs ys)
+end
+
+let rec append_val xs ys = xs |> Lazy.map_val begin function
+| Nil -> Lazy.force ys
+| Cons (x, xs) -> Cons (x, append_val xs ys)
+end
+
+let rec filter pred xs = lazy begin match xs with
+| lazy Nil -> Nil
+| lazy (Cons (x, xs)) ->
+  let xs = filter pred xs in
+  if pred x then Cons (x, xs) else Lazy.force xs
+end
+
+let rec combine xs ys = lazy begin match xs, ys with
+| lazy Nil, lazy Nil -> Nil
+| lazy (Cons (x, xs)), lazy (Cons (y, ys)) -> Cons ((x, y), combine xs ys)
+| _ -> invalid_arg "unequal lengths"
+end
---
+++ basis/data/lazy_list.mli
@@ -0,0 +1,113 @@
+type +'a node =
+| Nil
+| Cons of 'a * 'a t
+
+and +'a t = 'a node Lazy.t
+
+(** [nil] is the empty list. *)
+val nil : 'a t
+
+(** [cons x xs] is a list with a head of [x] and a tail of [xs]. *)
+val cons : 'a -> 'a t -> 'a t
+
+(** [lcons x xs] is a list with a head of [Lazy.force x] and a tail of [xs], but
+    it does not force the suspension [x] until the head of [lcons x xs] is
+    forced. *)
+val lcons : 'a Lazy.t -> 'a t -> 'a t
+
+(** [singleton x] is [cons x nil]. *)
+val singleton : 'a -> 'a t
+
+(** [lsingleton x] is [lcons x nil]. *)
+val lsingleton : 'a Lazy.t -> 'a t
+
+(** [is_nil xs] is [true] if [xs] is {!val:nil} and [false] otherwise. This
+    function forces the head of the list. *)
+val is_nil : 'a t -> bool
+
+(** [hd xs] is the head of [xs] or @raise Failure if [xs] is empty. *)
+val hd : 'a t -> 'a
+
+(** [hd_opt] is like {!val:hd} but it returns [None] rather than raising an
+    exception. *)
+val hd_opt : 'a t -> 'a option
+
+(** [tl xs] is the tail of [xs] or @raise Failure if [xs] is empty. *)
+val tl : 'a t -> 'a t
+
+(** [tl_opt] is like {!val:tl} but it returns [None] rather than raising an
+    exception. *)
+val tl_opt : 'a t -> 'a t option
+
+(** [at i xs] is the [i]th element of [xs]. This function forces the first [i]
+    elements of [xs].
+
+    @raise Not_found if [xs] does not have at least [i] elements
+  *)
+val at : int -> 'a t -> 'a
+
+(** [at_opt] is like {!val:at} but it returns [None] rather than raising an 
+    exception. *)
+val at_opt : int -> 'a t -> 'a option
+
+(** [nth xs i] is [at i xs]. *)
+val nth : 'a t -> int -> 'a
+
+(** [nth_opt xs i] is [at_opt i xs]. *)
+val nth_opt : 'a t -> int -> 'a option
+
+(** [fold_left f init xs] is [f (... (f (f init x1) x2) ...) xn] where [x1],
+    [x2], ..., [xn] denote consecutive elements of [xs]. This function is eager
+    and tail-recursive. *)
+val fold_left : ('acc -> 'a -> 'acc) -> 'acc -> 'a t -> 'acc
+
+(** [fold_right f xs init] is [f x1 (f x2 (... (f xn init) ...))] where [x1],
+    [x2], ..., [xn] denote consecutive elements of [xs]. This function is eager
+    and not tail-recursive. *)
+val fold_right : ('a -> 'acc -> 'acc) -> 'a t -> 'acc -> 'acc
+
+(** [length xs] is the number of elements in [xs]. This function is eager, and
+    will force the entire list. *)
+val length : 'a t -> int
+
+(** [iter f xs] is [f x1; f x2; ...; f xn] where [x1], [x2], ..., [xn] denote
+    consecutive elements of [xs]. This function is eager and tail-recursive. *)
+val iter : ('a -> unit) -> 'a t -> unit
+
+(** [iteri] is like {!val:iter}, but the function is also given the index of
+    each element. *)
+val iteri : (int -> 'a -> unit) -> 'a t -> unit
+
+(** [map f xs] is [cons (f x1) (cons (f x2) (... (cons (f xn) nil)))]. This
+    function is lazy. *)
+val map : ('a -> 'b) -> 'a t -> 'b t
+
+(** [mapi] is like {!val:map} but the function is also provided the index of the
+    element of the list. *)
+val mapi : (int -> 'a -> 'b) -> 'a t -> 'b t
+
+(** [map_val f xs] is like [map f xs], but if [xs] was constructed only from
+    immediate values (e.g., using only {!val:cons} and {!val:nil}), then it is
+    evaluated eagerly. If [xs] was only partially constructed using immediate
+    values, then this function is only partially eager. *)
+val map_val : ('a -> 'b) -> 'a t -> 'b t
+
+(** [mapi_val] is like {!val:map_val} but the function is also provided with the
+    index of each element in the list. *)
+val mapi_val : (int -> 'a -> 'b) -> 'a t -> 'b t
+
+(** [append xs ys] is a list containing all the elements of [xs] and then all
+    the elements of [ys]. This function is lazy. *)
+val append : 'a t -> 'a t -> 'a t
+
+(** [append_val] is like {!val:append}, but it is possibly-eager in the same way
+    as {!val:map_val}. *)
+val append_val : 'a t -> 'a t -> 'a t
+
+(** [filter pred xs] is [xs] without any element [x] for which [pred x] is
+    [false]. This function is lazy. *)
+val filter : ('a -> bool) -> 'a t -> 'a t
+
+(** [combine xs ys] is [cons (x1, y1) (cons (x2, y2) (... (cons (xn, yn) nil)))]
+    or @raise Invalid_arg if [xs] and [ys] are of unequal lengths. *)
+val combine : 'a t -> 'b t -> ('a * 'b) t
