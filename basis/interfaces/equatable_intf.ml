30bf567344af4f1fbd5bf79fdaff3c9167f1c460
Jordy Dickinson <jordy.dickinson@icloud.com>
2021-10-28 17:05:39 -0400

    Extend Equatable for parametric types

--- basis/interfaces/equatable_intf.ml
+++ basis/interfaces/equatable_intf.ml
@@ -12,6 +12,21 @@ module type Basic = sig
   val equal : t -> t -> bool
 end
 
+(** An interface for types which have some notion of equality. *)
+module type Basic1 = sig
+
+  type 'a t
+
+  (** [equal a b] is [true] if [a] and [b] are equal and [false] otherwise.
+
+      This function must satisfy the constraint that [a = b] implies
+      [equal a b], but otherwise the meaning of equality is up to the
+      implementation of this interface.
+    *)
+  val equal : 'a t -> 'a t -> bool
+end
+
+
 (** Infix equality operators *)
 module type Infix = sig
 
@@ -24,8 +39,28 @@ module type Infix = sig
   val ( <> ) : t -> t -> bool
 end
 
+
+(** Infix equality operators *)
+module type Infix1 = sig
+
+  type 'a t
+
+  (** [( = )] is an alias for [equal]. *)
+  val ( = ) : 'a t -> 'a t -> bool
+
+  (** [( <> )] is the logical negation of [( = )]. *)
+  val ( <> ) : 'a t -> 'a t -> bool
+end
+
 (** An interface for types which have some notion of equality. *)
 module type S = sig
   type t
   include Basic with type t := t
 end
+
+(** An interface for types which have some notion of equality. *)
+module type S1 = sig
+  type 'a t
+  include Basic1 with type 'a t := 'a t
+end
+
