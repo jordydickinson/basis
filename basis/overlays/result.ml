66c8675db53418c280e0638f60d22381830ec18b
Jordy Dickinson <jordy.dickinson@icloud.com>
2024-01-12 15:13:59 -0500

    Add Result.all_errors

--- basis/overlays/result.ml
+++ basis/overlays/result.ml
@@ -16,3 +16,23 @@ include Monad.Make2 (T)
 (* [Stdlib] has the arguments here in the wrong order, but to keep the peace we
    repeat the sins of our predecessors. *)
 let bind x f = bind f x
+
+let all_errors xs =
+  let rec all_errors' acc = function
+  | [] ->
+    begin match acc with
+    | Ok xs -> Ok (List.rev xs)
+    | Error es -> Error es
+    end
+  | Ok x :: xs ->
+    begin match acc with
+    | Ok xs' -> Ok (x :: xs')
+    | Error _ as acc -> all_errors' acc xs
+    end
+  | Error es :: xs ->
+    begin match acc with
+    | Ok _ -> all_errors' (Error es) xs
+    | Error es' -> all_errors' (Error (es' @ es)) xs
+    end
+  in
+  all_errors' (Ok []) xs
--- basis/overlays/result.mli
+++ basis/overlays/result.mli
@@ -1,2 +1,4 @@
 include Monad.S2 with type ('a, 'e) t := ('a, 'e) result
 include module type of struct include Stdlib.Result end
+
+val all_errors: ('a, 'e list) t list -> ('a list, 'e list) t
